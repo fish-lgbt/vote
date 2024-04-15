72c84311eb424f9c9244076f1c044d125e50f69b
luna <luna@wvvw.me>
2024-04-15 22:09:47 +0930

    fix: metadata base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,19 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/components/providers';
+import { headers } from 'next/headers';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Vote',
-  description:
-    'Vote and shape statistics on a variety of topics! Your voice matters. Join us in making informed decisions. Explore and participate today',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Vote',
+    description:
+      'Vote and shape statistics on a variety of topics! Your voice matters. Join us in making informed decisions. Explore and participate today',
+    // dynamically get the host from the Next headers
+    metadataBase: new URL(`https://${headers().get('host')}`),
+  };
+}
 
 export default function RootLayout({
   children,
