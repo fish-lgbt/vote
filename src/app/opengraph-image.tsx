6b49874f6538fa928bfbedaa758eadfab8bab7c1
luna <luna@wvvw.me>
2024-04-15 21:25:48 +0930

    feat: add OG image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,44 @@
+import { ImageResponse } from 'next/og';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const alt = 'Vote';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+// Image generation
+export default async function Image() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          fontFamily: 'monospace',
+          textTransform: 'uppercase',
+          color: 'black',
+          background: '#00ff95',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Start voting
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  );
+}
